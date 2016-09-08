a1133ce0739cd40adeab29a0f3d328b42ffc0f41
Jean <jean.jacobi@live.de>
2016-09-08 23:09:34 +0200

    Fix bug with counter

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/07 19:51:48 by                   #+#    #+#             */
-/*   Updated: 2016/09/08 22:50:46 by                  ###   ########.fr       */
+/*   Updated: 2016/09/08 23:08:27 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	index_str = 0;
 	if (needle[0] == '\0')
 		return ((char*)haystack);
+	if (len > ft_strlen(needle))
+		return ((char*)0);
 	while (haystack[index_str] != '\0')
 	{
 		index_tf = 0;
 		while (haystack[index_str + index_tf] == needle[index_tf])
-			if (needle[++index_tf] == '\0' || index_tf == len)
+			if (++index_tf == len)
 				return ((char*)&haystack[index_str]);
 		index_str++;
 	}
