68dedd98f040e9fa408ad745cbdef833a390fce1
Jean <jean.jacobi@live.de>
2016-09-08 22:35:59 +0200

    Fix bug : Empty strings and return a cast of big

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/07 19:14:05 by                   #+#    #+#             */
-/*   Updated: 2016/09/07 19:25:21 by                  ###   ########.fr       */
+/*   Updated: 2016/09/08 22:33:08 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,19 @@ char	*ft_strstr(const char *haystack, const char *needle)
 {
 	size_t	index_str;
 	size_t	index_tf;
-	char	*result;
 
 	index_str = 0;
+	if (!needle)
+		return ((char*)haystack);
+	if (!haystack)
+		return (NULL);
 	while (haystack[index_str] != '\0')
 	{
 		index_tf = 0;
 		while (haystack[index_str + index_tf] == needle[index_tf])
-		{
 			if (needle[++index_tf] == '\0')
-			{
-				result = ft_strdup(haystack);
-				return (&result[index_str]);
-			}
-		}
+				return ((char*)&haystack[index_str]);
 		index_str++;
 	}
-	return (0);
+	return (NULL);
 }
