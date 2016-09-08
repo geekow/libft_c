b3b94609f5bc73ef5fcf9d4ef6219db00b0a9200
Jean <jean.jacobi@live.de>
2016-09-08 22:51:56 +0200

    Error fix : Inspiring from ft_strstr error fix commit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/07 19:51:48 by                   #+#    #+#             */
-/*   Updated: 2016/09/07 20:15:35 by                  ###   ########.fr       */
+/*   Updated: 2016/09/08 22:50:46 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	index_str;
 	size_t	index_tf;
-	char	*result;
 
 	index_str = 0;
+	if (needle[0] == '\0')
+		return ((char*)haystack);
 	while (haystack[index_str] != '\0')
 	{
 		index_tf = 0;
 		while (haystack[index_str + index_tf] == needle[index_tf])
-		{
 			if (needle[++index_tf] == '\0' || index_tf == len)
-			{
-				result = ft_strdup(haystack);
-				return (&result[index_str]);
-			}
-		}
+				return ((char*)&haystack[index_str]);
 		index_str++;
 	}
-	return (0);
+	return ((char*)0);
 
 }
