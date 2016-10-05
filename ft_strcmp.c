7b62d7358e2f00146dbeaffcc549ecf1274740b6
Jean Jacobi <jjacobi@student.42.fr>
2016-10-05 21:52:37 +0200

    Non ascii support added

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/07 18:26:28 by                   #+#    #+#             */
-/*   Updated: 2016/09/07 18:31:34 by                  ###   ########.fr       */
+/*   Updated: 2016/10/05 21:50:19 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 	index = 0;
 	while (s1[index] == s2[index])
-	{
-		if (s1[index] == '\0')
+		if (s1[index++] == '\0')
 			return (0);
-		index++;
-	}
-	result = s1[index] - s2[index];
+	result = (unsigned char)s1[index] - (unsigned char)s2[index];
 	return (result);
 }
