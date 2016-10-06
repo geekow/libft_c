1527f0ac5e253be384c9172077f3dd1662bde4ca
Jean Jacobi <jjacobi@student.42.fr>
2016-10-07 00:05:20 +0200

    Fix bug

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/01 12:34:55 by                   #+#    #+#             */
-/*   Updated: 2016/09/01 17:02:55 by                  ###   ########.fr       */
+/*   Updated: 2016/10/07 00:04:40 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,11 @@
 void	*ft_memalloc(size_t size)
 {
 	void	*result;
-	char	*tmp;
 
 	result = malloc(size);
 	if (result)
 	{
-		tmp = (char*)result;
-		while ((size_t)tmp < size)
-		{
-			if (tmp)
-				*tmp = '\0';
-			tmp++;
-		}
+		ft_bzero(result, size);
 		return (result);
 	}
 	else
