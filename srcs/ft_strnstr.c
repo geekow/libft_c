1c35a3764026f59527c667945980c2c3eec031ac
Jean Jacobi <jjacobi@student.42.fr>
2016-11-05 18:36:29 +0100

    Fix function bug

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/03 23:03:34 by jjacobi           #+#    #+#             */
-/*   Updated: 2016/11/05 15:02:59 by jjacobi          ###   ########.fr       */
+/*   Updated: 2016/11/05 18:35:26 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	index_str = 0;
 	if (needle[0] == '\0' || haystack == needle)
 		return ((char*)haystack);
-	while (haystack[index_str] != '\0')
+	while (haystack[index_str] && len)
 	{
 		index_tf = 0;
+		len--;
 		while (haystack[index_str + index_tf] == needle[index_tf])
-			if (index_tf++ == len || index_tf == ft_strlen(needle))
-			{
-				if (len > ft_strlen(needle))
-					return ((char *)&haystack[index_str]);
-				else
-					return ((char*)0);
-			}
+		{
+			if ((len - index_tf) <= 0)
+				return ((char*)0);
+			if (needle[++index_tf] == '\0')
+				return ((char*)&haystack[index_str]);
+		}
 		index_str++;
 	}
 	return ((char*)0);
