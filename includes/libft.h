bfbbfcfbdc54dd8342f02d1aa354bc1d6e0bd35e
Jean Jacobi <jjacobi@student.42.fr>
2016-11-06 15:57:00 +0100

    New function : ft_lstadd

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/03 23:05:59 by jjacobi           #+#    #+#             */
-/*   Updated: 2016/11/06 15:30:18 by                  ###   ########.fr       */
+/*   Updated: 2016/11/06 15:54:38 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ void				ft_putnbr_fd(int n, int fd);
 void				ft_bzero(void *s, size_t n);
 void  				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
 void				*ft_memchr(const void *s, int c, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
