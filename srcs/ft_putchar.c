10370e50c4c2d6a13f46f2edac6226f13dba4fc6
Jean Jacobi <jjacobi@student.42.fr>
2017-01-10 07:16:15 +0100

    Unicode support

--- srcs/ft_putchar.c
+++ srcs/ft_putchar.c
@@ -6,13 +6,35 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/03 22:56:33 by jjacobi           #+#    #+#             */
-/*   Updated: 2016/11/03 22:56:34 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/01/10 07:15:47 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar(char c)
+void	ft_putchar(unsigned int c)
 {
-	write(1, &c, 1);
+	char	r[4];
+	size_t	index;
+
+	index = 0;
+	if ((r[0] = (char)c) && c > 65535)
+	{
+		r[index++] = 240 | ((c >> 18));
+		r[index++] = 128 | ((c >> 12) & 63);
+		r[index++] = 128 | ((c >> 6) & 63);
+		r[index] = 128 | (c & 63);
+	}
+	else if (c > 2047)
+	{
+		r[index++] = 224 | (c >> 12);
+		r[index++] = 128 | ((c >> 6) & 63);
+		r[index] = 128 | (c & 63);
+	}
+	else if (c > 127)
+	{
+		r[index++] = 192 | (c >> 6);
+		r[index] = 128 | (c & 63);
+	}
+	write(1, &r, ++index);
 }
